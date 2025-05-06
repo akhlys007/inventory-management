00957250086792172230279e9277d057978613e9
Timothy Chelelgo <timmybarkuch007@gmail.com>
2025-05-06 16:31:09 +0300

    -Added images from s3 bucket

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -4,8 +4,8 @@
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
-
 const Navbar = () => {
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
@@ -65,6 +65,13 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
+            <Image 
+            src="https://s3-inventorymanagement69.s3.us-east-1.amazonaws.com/profile.jpg"
+            alt="Profile"
+            width={50}
+            height={50}
+            className="rounded-full h-full object-cover"
+            />
             <span className="font-semibold">Tim Chelelgo</span>
           </div>
         </div>
