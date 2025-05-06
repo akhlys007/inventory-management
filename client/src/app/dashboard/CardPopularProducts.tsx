4601e4459f8ef288a94132d951e8da5ad6f41e2e
Timothy Chelelgo <timmybarkuch007@gmail.com>
2025-05-06 16:46:59 +0300

    -Edited favicon and title

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -23,12 +23,12 @@ const CardPopularProducts = () => {
                     >
                         <div className="flex items-center gap-3">
                         <Image
-                                            src={`https://s3-inventorymanagement69.s3.us-east-1.amazonaws.com/product${Math.floor(Math.random() * 3) + 1}.png`}
-                                            alt="product.name"
-                                            width={48}
-                                            height={48}
-                                            className="rounded-lg w-14 h-14"
-                                            />
+                        src={`https://s3-inventorymanagement69.s3.us-east-1.amazonaws.com/product${Math.floor(Math.random() * 3) + 1}.png`}
+                        alt="product.name"
+                        width={48}
+                        height={48}
+                        className="rounded-lg w-14 h-14"
+                        />
                         <div className="flex flex-col justify-between gap-1">
                         <div className="font-bold text-gray-700">{product.name}</div>
                         <div className="flex text-sm items-center">
